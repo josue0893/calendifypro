153f1e6baae716198f843d3ff18bf9aa424191cb
josue0893 <josue.bh8@icloud.com>
2025-08-06 17:28:27 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,10 @@
 // Configuración de Firebase
+const firebaseConfig = {
+   apiKey: "AIzaSyBYYozce_azUr3lMWpJ9DyWSTWSueVMq-I",
+  authDomain: "calendify-pro.firebaseapp.com",
+  projectId: "calendify-pro",
+  storageBucket: "calendify-pro.firebasestorage.app",
+  messagingSenderId: "559169595248",
+  appId: "1:559169595248:web:83b7f584ebb879b4b3d1b5"
+};
+firebase.initializeApp(firebaseConfig);
